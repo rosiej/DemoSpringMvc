bb3f2d07e64ff6e09a69701d2dd563d3fc71f1a3
Rosiej <mateusz.rosiejka@gmail.com>
2018-07-05 19:47:56 +0200

    search page added

--- demospringmvc/src/main/java/pl/rosiejka/demospringmvc/controller/TweetController.java
+++ demospringmvc/src/main/java/pl/rosiejka/demospringmvc/controller/TweetController.java
@@ -7,11 +7,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class TweetController {
@@ -20,6 +20,22 @@ public class TweetController {
     private Twitter twitter;
 
     @RequestMapping("/")
+    public String home() {
+        return "searchPage";
+    }
+
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+        if(search.toLowerCase().contains("porn")){
+            redirectAttributes.addFlashAttribute("error","nie no dajże spokój..");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search",search);
+        return "redirect:result";
+    }
+
+    @RequestMapping("/result")
     public String helloWorld(@RequestParam(defaultValue = "Tajniki Spring MVC4") String search, Model model){
 
         SearchResults searchResults = twitter.searchOperations().search(search);
