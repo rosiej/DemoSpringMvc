09b1be411351f0f7f8a29f49d3122f26f01a4c9a
Rosiej <mateusz.rosiejka@gmail.com>
2018-07-10 20:18:58 +0200

    profile form

--- demospringmvc/src/main/java/pl/rosiejka/demospringmvc/config/WebConfiguration.java
+++ demospringmvc/src/main/java/pl/rosiejka/demospringmvc/config/WebConfiguration.java
@@ -1,8 +1,12 @@
 package pl.rosiejka.demospringmvc.config;
 
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import pl.rosiejka.demospringmvc.date.USLocalDateFormatter;
 
 import java.time.LocalDate;
@@ -13,5 +17,15 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
+    }
+    @Bean
+    public LocaleResolver localResolver(){
+        return new SessionLocaleResolver();
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
     }
 }
--- demospringmvc/src/main/java/pl/rosiejka/demospringmvc/profile/ProfileController.java
+++ demospringmvc/src/main/java/pl/rosiejka/demospringmvc/profile/ProfileController.java
@@ -7,17 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.rosiejka.demospringmvc.date.USLocalDateFormatter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
 public class ProfileController {
 
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
+    }
+
     @RequestMapping("/profile")
     public String displayProfile(ProfileFormDTO profileFormDTO){
         return "profile/profileFormPage";
     }
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    @RequestMapping(value = "/profile",params = {"save"}, method = RequestMethod.POST)
     public String saveProfile(@Valid ProfileFormDTO profileFormDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()){
             return "profile/profileFormPage";
@@ -26,8 +32,18 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
-    @ModelAttribute("dateFormat")
-    public String localeFormat(Locale locale) {
-        return USLocalDateFormatter.getPattern(locale);
+    @RequestMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(ProfileFormDTO profileFormDTO) {
+        profileFormDTO.getTastes().add(null);
+        return "profile/profileFormPage";
     }
+
+    @RequestMapping(value = "/profile", params = {"removeTaste"})
+    public String removeRow(ProfileFormDTO profileFormDTO, HttpServletRequest request){
+        Integer rowId= Integer.valueOf(request.getParameter("removeTaste"));
+        profileFormDTO.getTastes().remove(rowId.intValue());
+        return "profile/profileFormPage";
+
+    }
+
 }
