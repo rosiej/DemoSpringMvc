6aaf94b5ff41342ee9945e4018c74a5b2ac6a2bd
Rosiej <mateusz.rosiejka@gmail.com>
2018-07-13 20:59:51 +0200

    page doesn't exist instead Whitelabel Error Page

--- demospringmvc/src/main/java/pl/rosiejka/demospringmvc/config/WebConfiguration.java
+++ demospringmvc/src/main/java/pl/rosiejka/demospringmvc/config/WebConfiguration.java
@@ -8,9 +8,11 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.util.UrlPathHelper;
 import pl.rosiejka.demospringmvc.date.USLocalDateFormatter;
 
 import java.time.LocalDate;
@@ -21,6 +23,12 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class, new USLocalDateFormatter());
+    }
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer){
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
     }
     @Bean
     public LocaleResolver localResolver(){
